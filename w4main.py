482a20c37bceda4d008429e7a0abd1b14625e354
Jongin Kim <anight08@gmail.com>
2016-03-28 13:52:46 +0900

    1
    1

---
+++ w4main.py
@@ -0,0 +1,22 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def MakeSwirlSquare(size):
+    sides=30
+    bigger=10
+    angle=90
+
+    t1.home()
+    t1.clear()
+
+    for i in range(0,sides):
+        if(i%3):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+
+MakeSwirlSquare(10)
+
+
+wn.exitonclick()
