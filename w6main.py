3cc7e71501e83ca0f10ff5263228328a7901fee9
김종인_200911208 <anight08@gmail.com>
2016-04-10 20:23:05 +0900

    Update w6main.py
    Been changed.

--- w6main.py
+++ w6main.py
@@ -2,6 +2,12 @@
 @author j.Kim
 @since 160406
 """
+
+import random
+"""
+This is for Up and down game at wk6
+"""
+
 def m35():
     result = 0
     for i in range(1,1000):
@@ -13,14 +19,27 @@ def m35():
 def a():
     year = float(input("year:"))
     if(year%4==0)and(year%100!=0 or year%400==0):
-        print "윤년"
+        print "Good"
     else:
-        print "윤년아님"
+        print "Yes it is a leap year"
+
+def UpAndDown():
+    num1=random.randrange(0,100)
+    for i in range(0,20):
+        num2=int(input("Try and guess a number between 1~100 : "))
+        if num1<num2:
+            print "Down"
+        elif num1>num2:
+            print "Up"
+        elif num1==num2:
+            print "You are right!"
+            break
         
 
 def lab6():
     print m35()
     a()
+    UpAndDown()
     
 def main():
     lab6()
