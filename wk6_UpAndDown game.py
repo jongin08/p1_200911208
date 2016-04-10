27cd7c6c4d2b6e948b61dd1d7bcabccbde76bb12
김종인_200911208 <anight08@gmail.com>
2016-04-10 20:19:04 +0900

    Added files via upload

---
+++ wk6_UpAndDown game.py
@@ -0,0 +1,15 @@
+﻿import random
+
+def UpAndDown():
+    num1=random.randrange(0,100)
+    for i in range(0,20):
+        num2=int(input("Try and guess a number between 1~100 : "))
+        if num1<num2:
+            print "Down"
+        elif num1>num2:
+            print "Up"
+        elif num1==num2:
+            print "You are right!"
+            break
+        
+UpAndDown()
