9c123ff04dfa962b4a84cd9fcee5590d44daa3cd
김종인_200911208 <anight08@gmail.com>
2016-04-12 13:22:49 +0900

    Added files via upload

---
+++ w7리스트main.py
@@ -0,0 +1,12 @@
+﻿x=list()
+def sumList(x):
+    sum=0
+    for i in range(1,1000):
+        if(i%4==0 and i%5!=0):
+            x.append(i)
+            
+    for i in range(0,len(x)):
+        sum=sum+x[i]
+    return sum
+
+sumList(x)
