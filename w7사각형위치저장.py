b9fba0e9957a027ff88cfe8558efbcc1649dd201
김종인_200911208 <anight08@gmail.com>
2016-04-18 13:41:47 +0900

    Added files via upload

---
+++ w7사각형위치저장.py
@@ -0,0 +1,21 @@
+﻿def drawSquareAt(size,pos):
+    t1.home()
+    t1.clear()
+    tracks=list()
+    t1.penup()
+    t1.goto(pos)
+    t1.pendown()
+    for i in range(0,4):
+        t1.fd(size)
+        t1.right(90)
+        tracks.append(t1.pos())
+    return tracks
+
+def lab7():
+   drawSquareAt(100,(0,0))
+
+def main():
+   lab7()
+
+if __name__=="__main__":
+    main()
