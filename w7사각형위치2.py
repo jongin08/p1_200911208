4ac7894a624d48ce58e8101ed3a358e8b9fa41fd
김종인_200911208 <anight08@gmail.com>
2016-04-19 17:28:54 +0900

    Added files via upload

---
+++ w7사각형위치2.py
@@ -0,0 +1,15 @@
+﻿import turtle
+wn = turtle.Screen()
+t1= turtle.Turtle()
+
+def drawSquareForm(tracks):   
+    t1.home()
+    for i in range(0,4):
+        t1.setpos(tracks[i])
+ 
+tracks=list()
+tracks.append((90.00,0.00))
+tracks.append((90.00,-90.00))
+tracks.append((00.00,-90.00))
+tracks.append((00.00,0.00))
+drawSquareForm(tracks)
