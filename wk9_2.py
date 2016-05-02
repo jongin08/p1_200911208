cebf6e632c76172ba0ceed13aa7174a60a07abd1
김종인_200911208 <anight08@gmail.com>
2016-05-02 13:40:18 +0900

    상명대 주소 문자 숫자로 표시

---
+++ wk9_2.py
@@ -0,0 +1,20 @@
+﻿%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+
+word = 'hongji 2 gil 20,Jongrogu,Seoul'
+
+z=dict()
+z['alpha']=0
+z['number']=0
+for i in word:
+    if i.isalpha():
+            z['alpha']=z['alpha']+1
+    elif i.isdigit:
+            z['number']=z['number']+1
+print z
+
+
+plt.bar(range(len(z)),z.values(),align='center')
+plt.xticks(range(len(z)),list(z.keys()))
+plt.show()
