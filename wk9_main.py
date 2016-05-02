54801ea5dd4c8bcea964ee8a4dad0dee689390e8
김종인_200911208 <anight08@gmail.com>
2016-05-02 13:18:10 +0900

    Added files via upload

---
+++ wk9_main.py
@@ -0,0 +1,32 @@
+﻿%matplotlib inline
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def charCount():
+    word=raw_input("")
+
+    for c in word:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]=d[c]+1
+    print d
+    
+
+def graph(charCount):
+
+    plt.bar(range(len(d)),d.values(),align='center')
+    plt.xticks(range(len(d)),list(d.keys()))
+    plt.show()
+    
+    
+def lab9():
+    d=dict()
+    charCount()
+    graph(charCount)
+def main():
+    lab9()
+    
+if __name__=="__main__":
+    main()
