d5ab2640db7ef87612449d85707175f2f4cb2f08
김종인_200911208 <anight08@gmail.com>
2016-05-03 19:32:51 +0900

    Update wk9_main.py

--- wk9_main.py
+++ wk9_main.py
@@ -2,10 +2,9 @@
 
 import matplotlib
 import matplotlib.pyplot as plt
-d=dict()
 
-def charCount(word):
-
+def CharCount(word):
+    d=dict()
     for c in word:
         if c not in d:
             d[c]=1
@@ -13,20 +12,48 @@ def charCount(word):
             d[c]=d[c]+1
     print d
     
-
-def graph():
-
+def CCgraph(word):
+    
     plt.bar(range(len(d)),d.values(),align='center')
     plt.xticks(range(len(d)),list(d.keys()))
     plt.show()
     
+def AlphaDigit(word):
+    e=dict()
+    e['alpha']=0
+    e['digit']=0
+    for c in word:
+        if c.isalpha()==True:
+            e['alpha']+=1
+        elif c.isdigit()==True:
+            e['digit']+=1    
+    print e     
+    
+    
+    plt.bar(range(len(e)), e.values(), align='center')
+    plt.xticks(range(len(e)), list(e.keys()))
+    plt.show()
+    
+
+    
+    
+def CC():
+    word='sangmyung university'
+    CharCount(word)
+    CCgraph(word)
+    
+def AD():
+    word="7 hongi,Jongro"
+    AlphaDigit(word)
+    
     
 def lab9():
-    word='sangmyung university'
-    charCount(word)
-    graph()
+    CC()
+    AD()
+    
 def main():
     lab9()
+    
     
 if __name__=="__main__":
     main()
