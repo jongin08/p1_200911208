ad9453ba2ec9b61fcd92bc07b91f4da950c4744a
김종인_200911208 <anight08@gmail.com>
2016-05-04 13:30:08 +0900

    최종수정

--- wk9_main.py
+++ wk9_main.py
@@ -2,9 +2,10 @@
 
 import matplotlib
 import matplotlib.pyplot as plt
+d=dict()
+e=dict()
 
-def CharCount(word):
-    d=dict()
+def CharCount(word):    
     for c in word:
         if c not in d:
             d[c]=1
@@ -19,7 +20,6 @@ def CCgraph(word):
     plt.show()
     
 def AlphaDigit(word):
-    e=dict()
     e['alpha']=0
     e['digit']=0
     for c in word:
@@ -30,6 +30,7 @@ def AlphaDigit(word):
     print e     
     
     
+def ADgraph(word):
     plt.bar(range(len(e)), e.values(), align='center')
     plt.xticks(range(len(e)), list(e.keys()))
     plt.show()
@@ -45,11 +46,44 @@ def CC():
 def AD():
     word="7 hongi,Jongro"
     AlphaDigit(word)
+    ADgraph(word)
+  
+
+   
+def friend_and_I():
+    i=set(['TV', 'phone', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'])
+    f=set(['coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'])
+    
+    print "  "
+    print "===1st==="
+    print i.union(f)
+    print "========="
+    print "===2nd==="
+    print i-f
+    print "========="
+    print "===3rd==="
+    print f-i
+    print "========="
+    print "===4th==="
+    print i.intersection(f)
+    print "========="
+
+    ee=dict()
+    for c in i.union(f):
+        if c not in i&f:
+            ee[c]=1
+        else:
+            ee[c]=2
+            
+    print "===5th==="
+    print ee
+    print "========="
     
     
 def lab9():
     CC()
     AD()
+    friend_and_I()
     
 def main():
     lab9()
