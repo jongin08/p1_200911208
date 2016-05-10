e2ac7434bf9adc6dd5bb0b43c7e8f5b434573d3f
김종인_200911208 <anight08@gmail.com>
2016-05-11 05:34:08 +0900

    10주차 최종
    과제들중 앞부분은 wk9에 들어가있어요.

--- wk10_main.py
+++ wk10_main.py
@@ -1,4 +1,4 @@
-﻿%matplotlib inline
+%matplotlib inline
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -38,10 +38,122 @@ def graph(pop):
     plt.xticks(range(len(d)), list(d.keys()))
     plt.show()
     
+    
+def coffeepot():
+    
+    coffee=[["Coffee","Water","Milk","Icecream"],
+            ["Espresso","No","No","No"],
+            ["Long Black","Yes","No","No"],
+            ["Flat White","No","Yes","No"],
+            ["Cappuccino","No","Yes","No"],
+            ["Affogato","No","No","Yes"]]
+
+    ingredient=coffee[1:6]
+    
+    milk=0
+    for i in range(0, len(ingredient)):
+        if (ingredient[i][2]=="Yes"):
+            milk+=1
+            print ingredient[i][0]
+    
+    print "milk =",float(milk)/len(ingredient)*100,"%"
+    
+
+def calGrade():
+    Class=[["English", "Math"],
+           [100, 200], 
+           [200, 200], 
+           [100, 300]]
+    
+    score=Class[1:4]
+    eSum=0
+    mSum=0
+    
+    for i in range(0, len(score)):
+        eSum=eSum+score[i][0]
+        mSum=mSum+score[i][1]
+        
+    eAvg=eSum/len(score)
+    mAvg=mSum/len(score)
+    
+    print "English :  Sum is",eSum,", and average is", eAvg
+    print "M a t h :  Sum is",mSum,", and average is", mAvg
+
+
+
+def max3word():
+    lyrics= [" When I find myself in times of trouble",
+            " Mother Mary comes to me",
+            " Speaking words of wisdom, let it be",
+            " And in my hour of darkness",
+            " She is standing right in front of me",
+            " Speaking words of wisdom, let it be",
+
+            " Let it be, let it be",
+            " Let it be, let it be",
+            " Whisper words of wisdom, let it be",
+
+            " And when the broken-hearted people",
+            " Living in the world agree",
+            " There will be an answer, let it be",
+            " For though they may be parted",
+            " There is still a chance that they will see",
+            " There will be an answer, let it be",
+
+            " Let it be, let it be",
+            " Let it be, let it be",
+            " Yeah, there will be an answer, let it be",
+            " Let it be, let it be",
+            " Let it be, let it be",
+            " Whisper words of wisdom, let it be",
+
+            " Let it be, let it be",
+            " Ah, let it be, yeah, let it be",
+            " Whisper words of wisdom, let it be",
+
+            " And when the night is cloudy",
+            " There is still a light that shines on me",
+            " Shine on until tomorrow, let it be",
+            " I wake up to the sound of music,",
+            " Mother Mary comes to me",
+            " Speaking words of wisdom, let it be",
+
+            " Let it be, let it be",
+            " Let it be, yeah, let it be",
+            " Oh, there will be an answer, let it be",
+            " Let it be, let it be",
+            " Let it be, yeah, let it be",
+            " Whisper words of wisdom, let it be"]
+    
+    
+    word=[]
+    
+    for i in range(0, len(lyrics)):
+        for j in range(0, len(lyrics[i].split())):
+            word.append(lyrics[i].split()[j])
+    
+    
+    d={}
+    
+    for c in word:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]+=1
+            
+    for h in d:
+        if(d[h]>=20):
+            print h, d[h]
+            
+            
 def lab10():
     pop=population()
     calPop(pop)
     graph(pop)
+    coffeepot()
+    calGrade()
+    max3word()
+
     
 def main():
     lab10()
